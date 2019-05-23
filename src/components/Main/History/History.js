8ac3e2ff01684804672d3dc99ea5bd72d2187ee3
marco981 <marco98x2@gmail.com>
2019-05-23 22:03:54 +0100

    create history component

--- src/components/Main/History/History.js
+++ src/components/Main/History/History.js
@@ -1,6 +1,28 @@
 import React from 'react'
 import styles from './History.module.css'
+import { FaHistory } from 'react-icons/fa'
 
-const history = props => <div className={styles.History}>History</div>
+const history = props => {
+  const entries = props.entries.map(entry => (
+    <li className={styles.HistoryEntryContainer}>
+      <div className={styles.HistoryEntry}>
+        <img src={entry.iconURL} alt={entry.iconAlt} />
+        <a href={entry.contentURL}>{entry.content}</a>
+      </div>
+      <div className={styles.HistoryDate}>{entry.date}</div>
+    </li>
+  ))
+  return (
+    <section className={styles.History}>
+      <div className={styles.HistoryLinks}>
+        <h6 className={styles.Title}>
+          <FaHistory style={{ marginRight: '.5rem' }} />
+          relevant javascript history
+        </h6>
+        {entries}
+      </div>
+    </section>
+  )
+}
 
 export default history
