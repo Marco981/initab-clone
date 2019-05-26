c0d01c97ab2a7b33fa120a44c54148e81c5047d1
marco981 <marco98x2@gmail.com>
2019-05-26 19:40:26 +0100

    fix issue of nested links in Post component

--- src/components/Main/Reddit/Post/Post.js
+++ src/components/Main/Reddit/Post/Post.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import styles from './Post.module.css'
 
 const post = props => (
-  <a href={props.link_content} className={styles.Post}>
-    <h6 className={styles.Content}>{props.content}</h6>
-    <p className={styles.PostedBy}>
-      Posted by: <span className={styles.User}>{props.user}</span>
-    </p>
-    <p className={styles.Score}>Reddit Score: {props.score}</p>
+  <Fragment>
+    <a href={props.link_content} className={styles.Post}>
+      <h6 className={styles.Content}>{props.content}</h6>
+      <p className={styles.PostedBy}>
+        Posted by: <span className={styles.User}>{props.user}</span>
+      </p>
+      <p className={styles.Score}>Reddit Score: {props.score}</p>
+    </a>
     <a className={styles.CommentLink} href={props.link_comment}>
       Link to Comments
     </a>
-  </a>
+  </Fragment>
 )
 
 export default post
